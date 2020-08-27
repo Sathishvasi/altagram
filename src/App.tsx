527cd3cb4b66e32f2f41db7d970ce0a825fc41ce
sathishvasi <sathish@wizroots.com>
2020-08-27 16:36:00 +0530

    Dropbox changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.scss";
 import Button from "./components/Button/Button";
+import Dropbox from "./components/Dropbox/Dropbox";
 
 function App() {
   return (
     <div className="app">
       <div className="app__container">
-        <Button>Hs</Button>
+        {/* <Button>Hs</Button> */}
+        <Dropbox />
       </div>
     </div>
   );
---
+++ src/components/Dropbox/Dropbox.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles/Dropbox.scss";
+
+function Dropbox() {
+  return (
+      <div>
+          Hi
+      </div>
+  );
+}
+
+export default Dropbox;
